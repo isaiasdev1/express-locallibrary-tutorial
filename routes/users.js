cbeaa2f956461a6707c3cd6c9474e697aba08bf6
Isaias-ml <isaias-ml@hotmail.com>
2018-12-06 15:32:42 -0300

    finish tutorial

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// GET users listing.
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/cool', function(req, res, next) {
-  res.send('Você é tão legal');
-});
-
 module.exports = router;
-
