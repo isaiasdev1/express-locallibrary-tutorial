12380b619c17393720157ef937607f102e6a22a0
Isaias-ml <isaias-ml@hotmail.com>
2018-12-08 22:19:16 -0300

    Finalizaçao de pesquisa

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -13,24 +13,23 @@ exports.search_result = function (req, res, next) {
     async.parallel({
         book: function (callback) {
             Book.find({
-                'title': { $regex: '.*' + busca + '.*' }
+                'title': { $regex: new RegExp(".*" + busca.toLowerCase() + '.*', "i") }
             }, 'title author ')
                 .populate('author').exec(callback);
         },
         author: function (callback) {
-            Author.find({ 'name': { $regex: '.*' + busca + '.*' } })
+            Author.find({ $or: [{ 'first_name': { $regex: new RegExp(".*" + busca.toLowerCase() + '.*', "i") } }, { 'family_name': { $regex: new RegExp(".*" + busca.toLowerCase() + '.*', "i") } }] })
                 .sort([['family_name', 'ascending']])
                 .exec(callback);
         },
         genre: function (callback) {
-            Genre.find({ 'name': { $regex: '.*' + busca + '.*' } })
+            Genre.find({ 'name': { $regex: new RegExp(".*" + busca.toLowerCase() + '.*', "i") } })
                 .sort([['name', 'ascending']])
                 .exec(callback);
         },
     }, function (err, results) {
         if (err) { return next(err); }
         // Successful, so render.
-        process.stdout.write(String(results.author) + '\n');
-        res.render('result_search', { title: 'Result Search', books: results.book, authors: results.author, genres: results.genre });
+        res.render('result_search', { title: 'Search Result', books: results.book, authors: results.author, genres: results.genre, busca: busca });
     });
 };
